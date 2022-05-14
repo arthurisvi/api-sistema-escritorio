746ef42e8e08ac7d033dace6cac5292ce239859d
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-05-14 16:17:29 -0300

    config: adjusting project configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomersModule } from './customers/customers.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_CONNECTION),
+  CustomersModule, ConfigModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
